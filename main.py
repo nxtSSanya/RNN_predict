3aee32f6b01ca9b178989d0754ff559385d1dcd8
Alexander <56893467+nxtSSanya@users.noreply.github.com>
2022-06-20 17:51:20 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,262 +1,156 @@
-import keras
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
+import math
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.layers import Dense, Dropout
+from keras.layers import BatchNormalization
+from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 
-from sklearn import preprocessing
-from sklearn.metrics import confusion_matrix, recall_score, precision_score
-from keras.models import Sequential,load_model
-from keras.layers import Dense, Dropout, LSTM
+for dirname, _, filenames in os.walk('\\kaggle\\input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
 
-# define path to save model
-model_path = 'binary_model.h5'
-train_df = pd.read_csv('PM_train.txt', sep=" ", header=None)
-train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
-train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
-                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
-                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+index_names = ['unit_nr', 'time_cycles']
+setting_names = ['setting_1', 'setting_2', 'setting_3']
+sensor_names = ['s_{}'.format(i) for i in range(1, 22)]
+col_names = index_names + setting_names + sensor_names
 
-train_df = train_df.sort_values(['id','cycle'])
-train_df.tail()
-test_df = pd.read_csv('PM_test.txt', sep=" ", header=None)
-test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
-test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
-                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
-                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
-test_df.head()
-truth_df = pd.read_csv('PM_truth.txt', sep=" ", header=None)
-truth_df.drop(truth_df.columns[[1]], axis=1, inplace=True)
-truth_df.head()
-rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
-rul.columns = ['id', 'max']
-train_df = train_df.merge(rul, on=['id'], how='left')
-train_df['RUL'] = train_df['max'] - train_df['cycle']
-train_df.drop('max', axis=1, inplace=True)
+# read data
+train = pd.read_csv('train_FD001.txt', sep='\s+', header=None, names=col_names)
+test = pd.read_csv('test_FD001.txt', sep='\s+', header=None, names=col_names)
+y_test = pd.read_csv('RUL_FD001.txt', sep='\s+', header=None, names=['RUL'])
 
-# Generate label columns for training data
-# we will only make use of "label1" for binary classification, while trying to answer the question: is a specific engine going to fail within w1 cycles?
-w1 = 30
-w0 = 15
-train_df['label1'] = np.where(train_df['RUL'] <= w1, 1, 0 )
-train_df['label2'] = train_df['label1']
-train_df.loc[train_df['RUL'] <= w0, 'label2'] = 2
-
-# MinMax normalization (from 0 to 1)
-train_df['cycle_norm'] = train_df['cycle']
-cols_normalize = train_df.columns.difference(['id','cycle','RUL','label1','label2'])
-min_max_scaler = preprocessing.MinMaxScaler()
-norm_train_df = pd.DataFrame(min_max_scaler.fit_transform(train_df[cols_normalize]),
-                             columns=cols_normalize,
-                             index=train_df.index)
-join_df = train_df[train_df.columns.difference(cols_normalize)].join(norm_train_df)
-train_df = join_df.reindex(columns = train_df.columns)
-train_df.head()
-# MinMax normalization (from 0 to 1)
-test_df['cycle_norm'] = test_df['cycle']
-norm_test_df = pd.DataFrame(min_max_scaler.transform(test_df[cols_normalize]),
-                            columns=cols_normalize,
-                            index=test_df.index)
-test_join_df = test_df[test_df.columns.difference(cols_normalize)].join(norm_test_df)
-test_df = test_join_df.reindex(columns = test_df.columns)
-test_df = test_df.reset_index(drop=True)
+train.head()
+test.head()
 
 
-# We use the ground truth dataset to generate labels for the test data.
-# generate column max for test data
-rul = pd.DataFrame(test_df.groupby('id')['cycle'].max()).reset_index()
-rul.columns = ['id', 'max']
-truth_df.columns = ['more']
-truth_df['id'] = truth_df.index + 1
-truth_df['max'] = rul['max'] + truth_df['more']
-truth_df.drop('more', axis=1, inplace=True)
+def add_remaining_useful_life(df):
+    # Get the total number of cycles for each unit
+    grouped_by_unit = df.groupby(by="unit_nr")
+    max_cycle = grouped_by_unit["time_cycles"].max()
 
-# generate RUL for test data
-test_df = test_df.merge(truth_df, on=['id'], how='left')
-test_df['RUL'] = test_df['max'] - test_df['cycle']
-test_df.drop('max', axis=1, inplace=True)
+    # Merge the max cycle back into the original frame
+    result_frame = df.merge(max_cycle.to_frame(name='max_cycle'), left_on='unit_nr', right_index=True)
 
-# generate label columns w0 and w1 for test data
-test_df['label1'] = np.where(test_df['RUL'] <= w1, 1, 0 )
-test_df['label2'] = test_df['label1']
-test_df.loc[test_df['RUL'] <= w0, 'label2'] = 2
-test_df.head()
-# pick a large window size of 50 cycles
-sequence_length = 50
+    # Calculate remaining useful life for each row
+    remaining_useful_life = result_frame["max_cycle"] - result_frame["time_cycles"]
+    result_frame["RUL"] = remaining_useful_life
+
+    # drop max_cycle as it's no longer needed
+    result_frame = result_frame.drop("max_cycle", axis=1)
+    return result_frame
 
 
-def gen_sequence(id_df, seq_length, seq_cols):
-    """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
-    we need to drop those which are below the window-length."""
-    # for one id we put all the rows in a single matrix
-    data_matrix = id_df[seq_cols].values
-    num_elements = data_matrix.shape[0]
-    # Iterate over two lists in parallel.
-    # For example id1 have 192 rows and sequence_length is equal to 50
-    # so zip iterate over two following list of numbers (0,112),(50,192)
-    for start, stop in zip(range(0, num_elements - seq_length), range(seq_length, num_elements)):
-        yield data_matrix[start:stop, :]
+train = add_remaining_useful_life(train)
+train[index_names + ['RUL']].head()
+
+drop_labels = index_names+setting_names
+#dropping the columns except the sensor datas
+X_train = train.drop(drop_labels, axis=1)
+y_train = X_train.pop('RUL')
+
+X_test = test.groupby('unit_nr').last().reset_index().drop(drop_labels, axis=1)
+
+X_train.shape
+
+from sklearn.model_selection import train_test_split
+from tensorflow.python.keras.layers import SimpleRNNCell
+from tensorflow.python.keras.layers import RNN
+X_trains, X_val, y_trains, y_val = train_test_split(X_train, y_train, test_size=0.2)
 
 
-# pick the feature columns
-sensor_cols = ['s' + str(i) for i in range(1, 22)]
-sequence_cols = ['setting1', 'setting2', 'setting3', 'cycle_norm']
-sequence_cols.extend(sensor_cols)
+model = keras.Sequential()
+model.add(Dense(21, activation='relu', input_shape=(21,)))
+model.add(BatchNormalization())
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1))
 
-# generator for the sequences
-seq_gen = (list(gen_sequence(train_df[train_df['id'] == id], sequence_length, sequence_cols))
-           for id in train_df['id'].unique())
+initial_learning_rate = 0.1
 
-# generate sequences and convert to numpy array
-seq_array = np.concatenate(list(seq_gen)).astype(np.float32)
-print("seq_array shape =", seq_array.shape)
-seq_array
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=100000,
+    decay_rate=0.96,
+    staircase=True)
+optimizedGradient = tf.compat.v1.train.GradientDescentOptimizer(
+    learning_rate=0.3, use_locking=False, name='GradientDescent'
+)
+optimizer_A = tf.keras.optimizers.Adam(learning_rate = 0.001)
+model.compile(optimizer=optimizer_A, loss='mean_absolute_error', metrics=['accuracy'])
+
+model.summary()
+
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                              patience=3, min_lr=1e-7, verbose=1)
+
+history = model.fit(x=X_train, y=y_train,
+                    validation_data = (X_val,y_val),
+                    epochs = 50,
+                    shuffle = True,
+                    callbacks=[reduce_lr])
 
 
-def gen_labels(id_df, seq_length, label):
-    # For one id we put all the labels in a single matrix.
-
-    data_matrix = id_df[label].values
-    num_elements = data_matrix.shape[0]
-
-    return data_matrix[seq_length:num_elements, :]
-
-
-# generate labels
-label_gen = [gen_labels(train_df[train_df['id'] == id], sequence_length, ['label1'])
-             for id in train_df['id'].unique()]
-label_array = np.concatenate(label_gen).astype(np.float32)
-print("label_array shape =", label_array.shape)
-label_array
-# build the network
-nb_features = seq_array.shape[2]
-nb_out = label_array.shape[1]
-
-model = Sequential()
-
-model.add(LSTM(
-         input_shape=(sequence_length, nb_features),
-         units=100,
-         return_sequences=True))
-model.add(Dropout(0.2))
-
-model.add(LSTM(
-          units=50,
-          return_sequences=False))
-model.add(Dropout(0.2))
-
-model.add(Dense(units=nb_out, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-print(model.summary())
-
-# fit the network
-history = model.fit(seq_array, label_array, epochs=100, batch_size=200, validation_split=0.05, verbose=2,
-          callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='min'),
-                       keras.callbacks.ModelCheckpoint(model_path,monitor='val_loss', save_best_only=True, mode='min', verbose=0)]
-          )
-
-# list all data in history
-print(history.history.keys())
-# summarize history for Accuracy
-fig_acc = plt.figure(figsize=(10, 10))
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy (Higher is better)')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+loss_train = history.history['loss']
+loss_val = history.history['val_loss']
+epochs = range(1, 51)
+plt.plot(epochs, loss_train, 'g', label='Training loss')
+plt.plot(epochs, loss_val, 'b', label='validation loss')
+plt.title('Training and Validation loss for Adam')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
 plt.show()
-fig_acc.savefig("model_accuracy.png")
 
-# summarize history for Loss
-fig_acc = plt.figure(figsize=(10, 10))
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss (Lower is better)')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+
+acc_train = history.history['accuracy']
+acc_val = history.history['val_accuracy']
+epochs = range(1,51)
+plt.plot(epochs, acc_train, 'r', label='Training accuracy')
+plt.plot(epochs, acc_val, 'g', label='validation accuracy')
+plt.title('Training and Validation Accuracy for Custom Arch.')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
 plt.show()
-fig_acc.savefig("model_loss.png")
 
-# training metrics
-scores = model.evaluate(seq_array, label_array, verbose=1, batch_size=200)
-print('Accurracy: {}'.format(scores[1]))
 
-# make predictions and compute confusion matrix
-y_pred = model.predict_classes(seq_array,verbose=1, batch_size=200)
-y_true = label_array
-print(y_pred.shape)
-print("\nPrediction on Validation Set is: ", y_pred)
+y_pred = model.predict(X_test)
+print(y_pred)
 
-test_set = pd.DataFrame(y_pred)
-test_set.to_csv('binary_submit_train.csv', index = None)
 
-print('Confusion matrix\n- x-axis is true labels.\n- y-axis is predicted labels')
-cm = confusion_matrix(y_true, y_pred)
-print(cm)
+import sklearn
+print(sklearn.metrics.r2_score(y_test, y_pred))
+#print(sklearn.metrics.mean_absolute_percentage_error(y_test, y_pred))
+print(sklearn.metrics.mean_absolute_error(y_test, y_pred))
 
-# compute precision and recall
-precision = precision_score(y_true, y_pred)
-recall = recall_score(y_true, y_pred)
-print( 'precision = ', precision, '\n', 'recall = ', recall)
-# We pick the last sequence for each id in the test data
+from autokeras import StructuredDataRegressor
 
-seq_array_test_last = [test_df[test_df['id']==id][sequence_cols].values[-sequence_length:]
-                       for id in test_df['id'].unique() if len(test_df[test_df['id']==id]) >= sequence_length]
+search = StructuredDataRegressor(max_trials=15, loss='mean_absolute_error') #no of trial and errors allowed
+search.fit(x=X_train, y=y_train, verbose=1) #fitting the model
 
-seq_array_test_last = np.asarray(seq_array_test_last).astype(np.float32)
-#print("seq_array_test_last")
-#print(seq_array_test_last)
-#print(seq_array_test_last.shape)
+mae, acc = search.evaluate(X_test, y_test, verbose=1)
 
-# Similarly, we pick the labels
+yhat = search.predict(X_test)
 
-# it is used to take only the labels of the sequences that are at least 50 long
-y_mask = [len(test_df[test_df['id']==id]) >= sequence_length for id in test_df['id'].unique()]
-print("y_mask")
-print(len(y_mask))
-label_array_test_last = test_df.groupby('id')['label1'].nth(-1)[y_mask].values
-label_array_test_last = label_array_test_last.reshape(label_array_test_last.shape[0],1).astype(np.float32)
-print(label_array_test_last.shape)
-#print("label_array_test_last")
-#print(label_array_test_last)
+print(sklearn.metrics.r2_score(y_test, yhat))
+print(sklearn.metrics.mean_absolute_error(y_test, yhat))
 
-# if best iteration's model was saved then load and use it
-if os.path.isfile(model_path):
-    estimator = load_model(model_path)
+model1 = search.export_model()
 
-# test metrics
-scores_test = estimator.evaluate(seq_array_test_last, label_array_test_last, verbose=2)
-print('Accurracy: {}'.format(scores_test[1]))
+model1.summary()
 
-# make predictions and compute confusion matrix
-y_pred_test = estimator.predict_classes(seq_array_test_last)
-y_true_test = label_array_test_last
-print(y_pred.shape)
-
-test_set = pd.DataFrame(y_pred_test)
-test_set.to_csv('binary_submit_test.csv', index = None)
-
-print('Confusion matrix\n- x-axis is true labels.\n- y-axis is predicted labels')
-cm = confusion_matrix(y_true_test, y_pred_test)
-print(cm)
-
-# compute precision and recall
-precision_test = precision_score(y_true_test, y_pred_test)
-recall_test = recall_score(y_true_test, y_pred_test)
-f1_test = 2 * (precision_test * recall_test) / (precision_test + recall_test)
-print( 'Precision: ', precision_test, '\n', 'Recall: ', recall_test,'\n', 'F1-score:', f1_test )
-
-# Plot in blue color the predicted data and in green color the
-# actual data to verify visually the accuracy of the model.
-fig_verify = plt.figure(figsize=(10, 5))
-plt.plot(y_pred_test, color="blue")
-plt.plot(y_true_test, color="green")
-plt.title('prediction')
-plt.ylabel('value')
-plt.xlabel('row')
-plt.legend(['predicted', 'actual data'], loc='upper left')
-plt.show()
-fig_verify.savefig("model_verify.png")
+model1.save('model1.tf') #saving the model
